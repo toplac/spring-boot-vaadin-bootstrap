c279aa148e1721856a928a51b64c7b920e0ddbb6
Tobias Placht <dev@knacht.net>
2014-08-14 01:42:51 +0200

    add initial sample implementation

---
+++ net.knacht.application.model/src/main/java/net/knacht/application/model/Person.java
@@ -0,0 +1,85 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.knacht.application.model;
+
+/**
+ * @author Tobias Placht (dev@knacht.net)
+ */
+
+public class Person {
+
+	private int id;
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Person other = (Person) obj;
+		if (id != other.id) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Person [id=").append(id).append(", name=").append(name)
+				.append("]");
+		return builder.toString();
+	}
+
+}
---
+++ net.knacht.application.ui.vaadin/src/main/java/net/knacht/application/ui/vaadin/views/MainView.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.knacht.application.ui.vaadin.views;
+
+import java.util.List;
+
+import net.knacht.application.model.Person;
+import net.knacht.application.service.api.IExampleService;
+import net.knacht.application.ui.vaadin.ui.ExampleUI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.vaadin.spring.navigator.VaadinView;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
+
+@Scope("prototype")
+@VaadinView(name = "main", ui = ExampleUI.class)
+public class MainView extends VerticalLayout implements View {
+
+	@Autowired
+	private IExampleService service;
+
+	@Override
+	public void enter(ViewChangeEvent event) {
+
+		List<Person> persons = service.findAll();
+
+		persons.forEach(p -> addComponent(new Label(String.format("ID: %s"
+				+ "\t" + "Name: %s", p.getId(), p.getName()))));
+	}
+
+}
